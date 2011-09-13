bd4f1359c63c2cade4deb9a083320a6e4cbb24ef
Jason Goldsmith <jason.goldsmith@sv.cmu.edu>
2011-09-12 17:30:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comicbin::Application.config.secret_token = '9d7bbf166daa4d86e70ab6f0b2431321fb3aeb2f0f604e017ac154872455f00d9af90469e520801728f3886661cd902f18ac638039d41dddfecdc4ff7445a610'
